28b01eb2a5ee409b00bd99b049c9542effebeec4
liu 35 <10756035@ntub.edu.tw>
2022-04-25 22:09:54 +0800

    資料庫連結2022/4/25

--- 111502_SystemCode(18)/app/src/main/java/com/system/MainActivity.java
+++ 111502_SystemCode(18)/app/src/main/java/com/system/MainActivity.java
@@ -24,10 +24,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void testonClick(View v){
         Thread thread = new Thread(mutiThread);
         thread.start();
     }
-
     public void ABonClick(View v) {
         Intent intent = new Intent();
         intent.setClass(MainActivity.this, MainActivityB.class);
@@ -48,56 +50,64 @@ public class MainActivity extends AppCompatActivity {
         intent.setClass(MainActivity.this, MainActivityO.class);
         startActivity(intent);
     }
-    private Runnable mutiThread = new Runnable() {
-        @Override
-        public void run() {
-            String result1 = null;
-            String result2 = null;
-            String result3 = null;
+    String result1 = null;
+    String result2 = null;
+    String result3 = null;
+    // 建立一個執行緒執行的事件取得網路資料
+    // Android 有規定，連線網際網路的動作都不能再主線程做執行
+    // 畢竟如果使用者連上網路結果等太久整個系統流程就卡死了
+    private Runnable mutiThread = new Runnable(){
+        public void run()
+        {
             try {
-                URL url = new URL("http://10.0.2.2/GetData.php");
+                URL url = new URL("http://127.0.0.1/GetData.php");
+                // 開始宣告 HTTP 連線需要的物件，這邊通常都是一綑的
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // 建立 Google 比較挺的 HttpURLConnection 物件
                 connection.setRequestMethod("POST");
-                connection.setDoOutput(true);
-                connection.setDoInput(true);
-                connection.setUseCaches(false);
-                connection.connect();
+                // 設定連線方式為 POST
+                connection.setDoOutput(true); // 允許輸出
+                connection.setDoInput(true); // 允許讀入
+                connection.setUseCaches(false); // 不使用快取
+                connection.connect(); // 開始連線
 
                 int responseCode = connection.getResponseCode();
-
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    InputStream inputStream = connection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"), 8);
-                    String line = null;
-                    while ((line = bufferedReader.readLine()) != null) {
+                // 建立取得回應的物件
+                if(responseCode == HttpURLConnection.HTTP_OK){ // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                    InputStream inputStream = connection.getInputStream(); // 取得輸入串流
+                    BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"), 8);
+                    // 讀取輸入串流的資料
+                    String line = null; // 宣告讀取用的字串
+                    while((line = bufReader.readLine()) != null) {
                         JSONArray dataJson = new JSONArray(line);
-                        int i = dataJson.length() - 1;
+                        int i = dataJson.length()-1;
                         JSONObject info = dataJson.getJSONObject(i);
-                        String time = info.getString("time");
-                        String dB = info.getString("dB");
-
-                        result1 = time.toString();
-                        result2 = dB.toString();
-
+                        String Username = info.getString("user_name"); // 宣告存放用字串
+                        String tag = info.getString("tag_");
+                        String content = info.getString("content");
+                        result1 = Username; // 把存放用字串放到全域變數
+                        result2 = tag;
+                        result3 = content;
                     }
-                    inputStream.close();
+                    inputStream.close(); // 關閉輸入串流
+
                 }
-            } catch (Exception e) {
-                result1 = e.toString();
+                // 讀取輸入串流並存到字串的部分
+                // 取得資料後想用不同的格式
+                // 例如 Json 等等，都是在這一段做處理
+
+            } catch(Exception e) {
+                result1 = e.toString(); // 如果出事，回傳錯誤訊息
                 result2 = e.toString();
                 result3 = e.toString();
             }
 
-            String finalResult1 = result1;
-            String finalResult2 = result2;
-            String finalResult3 = result3;
+            // 當這個執行緒完全跑完後執行
             runOnUiThread(new Runnable() {
-                @Override
                 public void run() {
-                    textViewUserNameA1.setText(finalResult1);
-                    textViewTagA1.setText(finalResult2);
-                    textViewTextA1.setText(finalResult3);
-
+                    textViewUserNameA1.setText(result1); // 更改顯示文字
+                    textViewTagA1.setText(result2);
+                    textViewTextA1.setText(result3);
                 }
             });
         }
